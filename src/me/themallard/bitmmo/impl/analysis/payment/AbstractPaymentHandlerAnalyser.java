2fac6a2c79b46412defa0347774a1441f9c1d895
Matthew Smith <matty@mattysmith.co.uk>
2015-08-08 15:04:20 +0100

    Identify Payment Handler's openWebpage() method
    Analyse method in payment handlers by their call to open a webpage.

--- src/me/themallard/bitmmo/impl/analysis/payment/AbstractPaymentHandlerAnalyser.java
+++ src/me/themallard/bitmmo/impl/analysis/payment/AbstractPaymentHandlerAnalyser.java
@@ -1,6 +1,12 @@
 package me.themallard.bitmmo.impl.analysis.payment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodInsnNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import me.themallard.bitmmo.api.analysis.Builder;
 import me.themallard.bitmmo.api.analysis.ClassAnalyser;
@@ -8,8 +14,9 @@ import me.themallard.bitmmo.api.analysis.IFieldAnalyser;
 import me.themallard.bitmmo.api.analysis.IMethodAnalyser;
 import me.themallard.bitmmo.api.analysis.SupportedHooks;
 import me.themallard.bitmmo.api.analysis.util.LdcContains;
+import me.themallard.bitmmo.api.hook.MethodHook;
 
-@SupportedHooks(fields = {}, methods = {})
+@SupportedHooks(fields = {}, methods = { "openWebpage&()V" })
 public abstract class AbstractPaymentHandlerAnalyser extends ClassAnalyser {
 	private String paymentUrl;
 
@@ -23,6 +30,31 @@ public abstract class AbstractPaymentHandlerAnalyser extends ClassAnalyser {
 		return LdcContains.ClassContains(cn, paymentUrl);
 	}
 
+	public class OpenWebpageAnalyser implements IMethodAnalyser {
+
+		@Override
+		public List<MethodHook> find(ClassNode cn) {
+			List<MethodHook> list = new ArrayList<MethodHook>();
+
+			for (MethodNode mn : cn.methods) {
+				for (AbstractInsnNode ain : mn.instructions.toArray()) {
+					if (!(ain instanceof MethodInsnNode))
+						continue;
+
+					MethodInsnNode min = (MethodInsnNode) ain;
+
+					if (min.name.equals("openURL")) {
+						list.add(asMethodHook(mn, "openWebpage"));
+						break;
+					}
+				}
+			}
+
+			return list;
+		}
+
+	}
+
 	@Override
 	protected Builder<IFieldAnalyser> registerFieldAnalysers() {
 		return null;
@@ -30,6 +62,6 @@ public abstract class AbstractPaymentHandlerAnalyser extends ClassAnalyser {
 
 	@Override
 	protected Builder<IMethodAnalyser> registerMethodAnalysers() {
-		return null;
+		return new Builder<IMethodAnalyser>().add(new OpenWebpageAnalyser());
 	}
 }
--- src/me/themallard/bitmmo/impl/analysis/payment/SteamOverlayPaymentHandlerAnalyser.java
+++ src/me/themallard/bitmmo/impl/analysis/payment/SteamOverlayPaymentHandlerAnalyser.java
@@ -1,7 +1,43 @@
 package me.themallard.bitmmo.impl.analysis.payment;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
+
+import me.themallard.bitmmo.api.analysis.Builder;
+import me.themallard.bitmmo.api.analysis.IMethodAnalyser;
+import me.themallard.bitmmo.api.analysis.util.LdcContains;
+import me.themallard.bitmmo.api.hook.MethodHook;
+
 public class SteamOverlayPaymentHandlerAnalyser extends AbstractPaymentHandlerAnalyser {
 	public SteamOverlayPaymentHandlerAnalyser() {
 		super("SteamOverlay", "https://8bitmmo.net/steam.php?overlay=true&do=igp&packID=");
+	}
+
+	// SteamOverlay handler does not have a call to open the web browser
+	// therefore we use a different method here
+	public class OpenWebpageAnalyser implements IMethodAnalyser {
+
+		@Override
+		public List<MethodHook> find(ClassNode cn) {
+			List<MethodHook> list = new ArrayList<MethodHook>();
+
+			for (MethodNode mn : cn.methods) {
+				if (LdcContains.MethodContains(mn, "Response from kicking off steam purchase url:")) {
+					list.add(asMethodHook(mn, "openWebpage"));
+					break;
+				}
+			}
+
+			return list;
+		}
+
+	}
+
+	@Override
+	protected Builder<IMethodAnalyser> registerMethodAnalysers() {
+		return new Builder<IMethodAnalyser>().add(new OpenWebpageAnalyser());
 	}
 }
