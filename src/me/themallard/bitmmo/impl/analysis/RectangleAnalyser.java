444cca7a76e6ed9fe6f68c1db395f9c47d2978a6
Matthew Smith <matty@mattysmith.co.uk>
2015-08-09 09:32:02 +0100

    Identify components of Rectangle.class
    Analyse the x, y, width and height fields of Rectangle.class

--- src/me/themallard/bitmmo/impl/analysis/RectangleAnalyser.java
+++ src/me/themallard/bitmmo/impl/analysis/RectangleAnalyser.java
@@ -1,5 +1,8 @@
 package me.themallard.bitmmo.impl.analysis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.objectweb.asm.tree.ClassNode;
 
 import me.themallard.bitmmo.api.analysis.Builder;
@@ -8,8 +11,9 @@ import me.themallard.bitmmo.api.analysis.IFieldAnalyser;
 import me.themallard.bitmmo.api.analysis.IMethodAnalyser;
 import me.themallard.bitmmo.api.analysis.SupportedHooks;
 import me.themallard.bitmmo.api.analysis.util.LdcContains;
+import me.themallard.bitmmo.api.hook.FieldHook;
 
-@SupportedHooks(fields = {}, methods = {})
+@SupportedHooks(fields = { "x&I", "y&I", "width&I", "height&I" }, methods = {})
 public class RectangleAnalyser extends ClassAnalyser {
 	public RectangleAnalyser() {
 		super("Rectangle");
@@ -20,9 +24,24 @@ public class RectangleAnalyser extends ClassAnalyser {
 		return LdcContains.ClassContains(cn, "Rectangle: ");
 	}
 
+	public class XYWHAnalyser implements IFieldAnalyser {
+		@Override
+		public List<FieldHook> find(ClassNode cn) {
+			List<FieldHook> list = new ArrayList<FieldHook>();
+
+			// Rectangle.class never changes so it is probably safe to do this
+			list.add(asFieldHook(cn.fields.get(0), "x"));
+			list.add(asFieldHook(cn.fields.get(1), "y"));
+			list.add(asFieldHook(cn.fields.get(2), "width"));
+			list.add(asFieldHook(cn.fields.get(3), "height"));
+
+			return list;
+		}
+	}
+
 	@Override
 	protected Builder<IFieldAnalyser> registerFieldAnalysers() {
-		return null;
+		return new Builder<IFieldAnalyser>().add(new XYWHAnalyser());
 	}
 
 	@Override
