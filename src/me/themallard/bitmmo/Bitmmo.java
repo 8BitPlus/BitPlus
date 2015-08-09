dd0324b27ea22a03dfc69bde513844b6f460c13d
Matthew Smith <matty@mattysmith.co.uk>
2015-08-09 21:50:50 +0100

    Add license headers and print a message on launch
    This resolves #4 by attaching the headers and also printing a message at
    startup.

--- src/me/themallard/bitmmo/Bitmmo.java
+++ src/me/themallard/bitmmo/Bitmmo.java
@@ -1,3 +1,18 @@
+/* Copyright (C) 2015 maaatts
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
 package me.themallard.bitmmo;
 
 import me.themallard.bitmmo.api.Context;
@@ -8,11 +23,15 @@ import me.themallard.bitmmo.impl.AnalysisProviderImpl;
 
 public class Bitmmo {
 	public static void main(String[] args) {
+		System.out.println("Bit+ Copyright (c) 2015 maaatts\n"
+				+ "This program comes with ABSOLUTELY NO WARRANTY; for details check LICENSE.md.\n"
+				+ "Tis is free software, and you are welcome to redistribute it under certain conditions.\n");
+
 		System.out.println("Loading 8BitMMO [" + RevisionHelper.getLatestRevision() + "]...");
 
 		try {
 			Revision latest = Revision.create(RevisionHelper.getLatestRevision());
-			
+
 			AbstractAnalysisProvider provider = new AnalysisProviderImpl(latest);
 			Context.bind(provider);
 			provider.run();
