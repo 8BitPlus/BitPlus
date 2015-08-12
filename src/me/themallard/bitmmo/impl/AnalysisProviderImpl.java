d08eecc4169cafeebed3029ab3e539971839cbcc
Matthew Smith <matty@mattysmith.co.uk>
2015-08-12 14:59:36 +0100

    Identify AudioManager.class
    Analyse AudioManager.class based on the strings that it contains.

--- src/me/themallard/bitmmo/impl/AnalysisProviderImpl.java
+++ src/me/themallard/bitmmo/impl/AnalysisProviderImpl.java
@@ -59,8 +59,8 @@ public class AnalysisProviderImpl extends AbstractAnalysisProvider {
 				new ReferFriendAnalyser(), new BuyGoldAnalyser(), new ShopAnalyser(), new SFXManagerAnalyser(),
 				new KeybindMenuAnalyser(), new LevelupPopupAnalyser(), new LeftMenuAnalyser(),
 				new BGMDownloaderAnalyser(), new TradeWindowAnalyser(), new BuildToolsAnalyser(),
-				new RectangleAnalyser(), new ChatWindowAnalyser(), new DirectionAnalyser(),
-				new InputHandlerAnalyser() });
+				new RectangleAnalyser(), new ChatWindowAnalyser(), new DirectionAnalyser(), new InputHandlerAnalyser(),
+				new AudioManagerAnalyser() });
 
 		return builder;
 	}
---
+++ src/me/themallard/bitmmo/impl/analysis/AudioManagerAnalyser.java
@@ -0,0 +1,49 @@
+/* Copyright (C) 2015 maaatts
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>. */
+
+package me.themallard.bitmmo.impl.analysis;
+
+import org.objectweb.asm.tree.ClassNode;
+
+import me.themallard.bitmmo.api.analysis.Builder;
+import me.themallard.bitmmo.api.analysis.ClassAnalyser;
+import me.themallard.bitmmo.api.analysis.IFieldAnalyser;
+import me.themallard.bitmmo.api.analysis.IMethodAnalyser;
+import me.themallard.bitmmo.api.analysis.SupportedHooks;
+import me.themallard.bitmmo.api.analysis.util.LdcContains;
+
+@SupportedHooks(fields = {}, methods = {})
+public class AudioManagerAnalyser extends ClassAnalyser {
+	public AudioManagerAnalyser() {
+		super("AudioManager");
+	}
+
+	@Override
+	protected boolean matches(ClassNode cn) {
+		return LdcContains.ClassContains(cn, "raw") && LdcContains.ClassContains(cn, ".au")
+				&& LdcContains.ClassContains(cn, ".wav") && LdcContains.ClassContains(cn, ".ogg")
+				&& LdcContains.ClassContains(cn, "pulpcore.sound.JOrbisAdapter");
+	}
+
+	@Override
+	protected Builder<IFieldAnalyser> registerFieldAnalysers() {
+		return null;
+	}
+
+	@Override
+	protected Builder<IMethodAnalyser> registerMethodAnalysers() {
+		return null;
+	}
+}
