e623543560dccdbb78c9c639d15d7db003a680dd
Matthew Smith <matty@mattysmith.co.uk>
2015-08-22 15:28:13 +0100

    Identify BGMDownloader instance and getInstance()
    Analyse instance field through its type, and create a pattern to find
    getInstance.

--- src/me/themallard/bitmmo/impl/analysis/BGMDownloaderAnalyser.java
+++ src/me/themallard/bitmmo/impl/analysis/BGMDownloaderAnalyser.java
@@ -15,7 +15,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 package me.themallard.bitmmo.impl.analysis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldInsnNode;
+import org.objectweb.asm.tree.FieldNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import me.themallard.bitmmo.api.analysis.Builder;
 import me.themallard.bitmmo.api.analysis.ClassAnalyser;
@@ -23,8 +29,15 @@ import me.themallard.bitmmo.api.analysis.IFieldAnalyser;
 import me.themallard.bitmmo.api.analysis.IMethodAnalyser;
 import me.themallard.bitmmo.api.analysis.SupportedHooks;
 import me.themallard.bitmmo.api.analysis.util.LdcContains;
+import me.themallard.bitmmo.api.analysis.util.pattern.Pattern;
+import me.themallard.bitmmo.api.analysis.util.pattern.PatternBuilder;
+import me.themallard.bitmmo.api.analysis.util.pattern.element.AnyElement;
+import me.themallard.bitmmo.api.analysis.util.pattern.element.FieldElement;
+import me.themallard.bitmmo.api.analysis.util.pattern.element.InstructionElement;
+import me.themallard.bitmmo.api.hook.FieldHook;
+import me.themallard.bitmmo.api.hook.MethodHook;
 
-@SupportedHooks(fields = {}, methods = {})
+@SupportedHooks(fields = { "instance&Lat;" }, methods = { "getInstance&()Lat;" })
 public class BGMDownloaderAnalyser extends ClassAnalyser {
 	public BGMDownloaderAnalyser() {
 		super("BGMDownloader");
@@ -35,13 +48,49 @@ public class BGMDownloaderAnalyser extends ClassAnalyser {
 		return LdcContains.ClassContains(cn, "BGM Music Download: Complete!");
 	}
 
+	public class InstanceAnalyser implements IFieldAnalyser {
+		@Override
+		public List<FieldHook> find(ClassNode cn) {
+			List<FieldHook> list = new ArrayList<FieldHook>();
+
+			for (FieldNode fn : cn.fields) {
+				String refactored = getRefactoredNameByType(fn.desc);
+
+				if (refactored == null)
+					continue;
+
+				if (refactored.equals("BGMDownloader"))
+					list.add(asFieldHook(fn, "instance"));
+			}
+
+			return list;
+		}
+	}
+
 	@Override
 	protected Builder<IFieldAnalyser> registerFieldAnalysers() {
-		return null;
+		return new Builder<IFieldAnalyser>().add(new InstanceAnalyser());
+	}
+
+	public class GetInstanceAnalyser implements IMethodAnalyser {
+		@Override
+		public List<MethodHook> find(ClassNode cn) {
+			List<MethodHook> list = new ArrayList<MethodHook>();
+
+			Pattern p = new PatternBuilder().add(new FieldElement(new FieldInsnNode(GETSTATIC, null, null, null)),
+					new InstructionElement(IFNONNULL), new AnyElement(), new InstructionElement(DUP)).build();
+
+			for (MethodNode mn : cn.methods) {
+				if (p.contains(mn.instructions))
+					list.add(asMethodHook(mn, "getInstance"));
+			}
+
+			return list;
+		}
 	}
 
 	@Override
 	protected Builder<IMethodAnalyser> registerMethodAnalysers() {
-		return null;
+		return new Builder<IMethodAnalyser>().add(new GetInstanceAnalyser());
 	}
 }
