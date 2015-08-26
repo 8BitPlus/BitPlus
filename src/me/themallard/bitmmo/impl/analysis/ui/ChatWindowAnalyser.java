b8e4d4eca0cd2a1151984a541a45a349fa3da03b
Matthew Smith <matty@mattysmith.co.uk>
2015-08-26 10:12:41 +0100

    Analyse ChatWindow.addChatMessage
    Identify addChatMessage through the strings that it contains.

--- src/me/themallard/bitmmo/impl/analysis/ui/ChatWindowAnalyser.java
+++ src/me/themallard/bitmmo/impl/analysis/ui/ChatWindowAnalyser.java
@@ -15,7 +15,11 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
 package me.themallard.bitmmo.impl.analysis.ui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
 import me.themallard.bitmmo.api.analysis.Builder;
 import me.themallard.bitmmo.api.analysis.ClassAnalyser;
@@ -23,8 +27,9 @@ import me.themallard.bitmmo.api.analysis.IFieldAnalyser;
 import me.themallard.bitmmo.api.analysis.IMethodAnalyser;
 import me.themallard.bitmmo.api.analysis.SupportedHooks;
 import me.themallard.bitmmo.api.analysis.util.LdcContains;
+import me.themallard.bitmmo.api.hook.MethodHook;
 
-@SupportedHooks(fields = {}, methods = {})
+@SupportedHooks(fields = {}, methods = { "addChatMessage&(Le;)V" })
 public class ChatWindowAnalyser extends ClassAnalyser {
 	public ChatWindowAnalyser() {
 		super("ui/ChatWindow");
@@ -40,8 +45,23 @@ public class ChatWindowAnalyser extends ClassAnalyser {
 		return null;
 	}
 
+	public class AddChatMessageAnalyser implements IMethodAnalyser {
+		@Override
+		public List<MethodHook> find(ClassNode cn) {
+			List<MethodHook> list = new ArrayList<MethodHook>();
+
+			for (MethodNode mn : cn.methods) {
+				if (LdcContains.MethodContains(mn, "\n") && LdcContains.MethodContains(mn, "\n<")
+						&& LdcContains.MethodContains(mn, "> "))
+					list.add(asMethodHook(mn, "addChatMessage"));
+			}
+
+			return list;
+		}
+	}
+
 	@Override
 	protected Builder<IMethodAnalyser> registerMethodAnalysers() {
-		return null;
+		return new Builder<IMethodAnalyser>().add(new AddChatMessageAnalyser());
 	}
 }
