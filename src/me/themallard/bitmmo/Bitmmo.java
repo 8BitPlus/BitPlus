f00973327820d79668c8a57fa698c0f0ccf189ef
Matthew Smith <matty@mattysmith.co.uk>
2015-09-02 12:08:29 +0100

    Prepare for 1.1.1 release
    Printed wrong release version, whoops.

--- src/me/themallard/bitmmo/Bitmmo.java
+++ src/me/themallard/bitmmo/Bitmmo.java
@@ -32,7 +32,7 @@ import me.themallard.bitmmo.impl.plugin.PluginLoader;
 import me.themallard.bitmmo.impl.transformer.TransformerRegistryImpl;
 
 public class Bitmmo {
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.1.1";
 
 	public static void main(String[] args) {
 		System.out.printf(
