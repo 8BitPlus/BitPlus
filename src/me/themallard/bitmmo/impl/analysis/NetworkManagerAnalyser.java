5b0fbfa225d2a0b777a39db8e95338f37e17de99
Matthew Smith <matty@mattysmith.co.uk>
2015-12-03 12:47:24 +0000

    Analyse NetworkManager's fields
    Identify
    	* NetworkManager.socket
    	* NetworkManager.outputStream
    	* NetworkManager.inputStream
    	* NetworkManager.dataOutputStream
    	* NetworkManager.dataInputStream
    
    Based on their types

--- src/me/themallard/bitmmo/impl/analysis/NetworkManagerAnalyser.java
+++ src/me/themallard/bitmmo/impl/analysis/NetworkManagerAnalyser.java
@@ -1,6 +1,10 @@
 package me.themallard.bitmmo.impl.analysis;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.FieldNode;
 import org.objectweb.asm.tree.LdcInsnNode;
 
 import me.themallard.bitmmo.api.analysis.Builder;
@@ -10,8 +14,11 @@ import me.themallard.bitmmo.api.analysis.IMethodAnalyser;
 import me.themallard.bitmmo.api.analysis.SupportedHooks;
 import me.themallard.bitmmo.api.analysis.util.pattern.PatternBuilder;
 import me.themallard.bitmmo.api.analysis.util.pattern.element.LdcElement;
+import me.themallard.bitmmo.api.hook.FieldHook;
 
-@SupportedHooks(fields = {}, methods = {})
+@SupportedHooks(fields = { "socket&Ljava/net/Socket;", "outputStream&Ljava/io/OutputStream;",
+		"inputStream&Ljava/io/InputStream;", "dataOutputStream&Ljava/io/DataOutputStream;",
+		"dataInputStream&Ljava/io/DataInputStream;" }, methods = {})
 public class NetworkManagerAnalyser extends ClassAnalyser {
 	private static final String[] STRINGS = { "serverName", "serverPort", "serverID", "Server " };
 
@@ -29,9 +36,40 @@ public class NetworkManagerAnalyser extends ClassAnalyser {
 		return true;
 	}
 
+	public class FieldTypeAnalyser implements IFieldAnalyser {
+		@Override
+		public List<FieldHook> find(ClassNode cn) {
+			List<FieldHook> list = new ArrayList<FieldHook>();
+
+			for (FieldNode fn : cn.fields) {
+				switch (fn.desc) {
+				case "Ljava/net/Socket;":
+					list.add(asFieldHook(fn, "socket"));
+					break;
+				case "Ljava/io/OutputStream;":
+					list.add(asFieldHook(fn, "outputStream"));
+					break;
+				case "Ljava/io/InputStream;":
+					list.add(asFieldHook(fn, "inputStream"));
+					break;
+				case "Ljava/io/DataOutputStream;":
+					list.add(asFieldHook(fn, "dataOutputStream"));
+					break;
+				case "Ljava/io/DataInputStream;":
+					list.add(asFieldHook(fn, "dataInputStream"));
+					break;
+				default:
+					break;
+				}
+			}
+
+			return list;
+		}
+	}
+
 	@Override
 	protected Builder<IFieldAnalyser> registerFieldAnalysers() {
-		return null;
+		return new Builder<IFieldAnalyser>().add(new FieldTypeAnalyser());
 	}
 
 	@Override
