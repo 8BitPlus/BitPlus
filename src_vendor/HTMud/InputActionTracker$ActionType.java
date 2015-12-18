84ab2d679da42b0e95690f976e5f042f145a3731
Matthew Smith <matty@mattysmith.co.uk>
2015-12-18 11:42:21 +0000

    Add the games Button Press enum to vendor package

---
+++ src_vendor/HTMud/InputActionTracker$ActionType.java
@@ -0,0 +1,7 @@
+// this is a stupid hack but it works
+
+package HTMud;
+
+public enum InputActionTracker$ActionType {
+	MOVE_NORTH, MOVE_EAST, MOVE_SOUTH, MOVE_WEST, USE, ATTACK, PICKUP, DROP, HALVE_TRADE, BUILD, INVENTORY, SHOP, ZOOM, LIGHT_THREAD;
+}
